5cb3e259ea1871b13b9ccf26c7b7e32678637c90
zhaoxinpeng <15531506906@sina.cn>
2018-08-19 19:49:59 +0800

    诸葛大脑 链接提交收录查询

--- zhugedanao/views_dir/Access_tijiaolianjie_task/access_task.py
+++ zhugedanao/views_dir/Access_tijiaolianjie_task/access_task.py
@@ -30,7 +30,7 @@ def decideIsTask(request):
     q = Q()
     next_datetime_addoneday = (datetime.datetime.now() - datetime.timedelta(minutes=30)).strftime('%Y-%m-%d %H:%M:%S')
     q.add(Q(create_date__lte=next_datetime_addoneday), Q.AND)
-    objs = models.zhugedanao_lianjie_tijiao.objects.filter(q).filter(is_zhixing=0)
+    objs = models.zhugedanao_lianjie_tijiao.objects.filter(q).filter(is_zhixing=0)[0:1]
     flag = False
     if objs:
         flag = True
@@ -105,8 +105,7 @@ def tiJiaoLianJieDecideIsTask(request):
     # status 收录状态
     q.add(Q(status=1) & Q(is_zhixing=1) & Q(submit_date__lte=next_datetime_addoneday), Q.AND)
     q.add(Q(time_stamp__isnull=True) | Q(time_stamp__lt=now_time_stamp), Q.AND)
-    objs = models.zhugedanao_lianjie_tijiao.objects.filter(q)
-    print('objs---> ', objs)
+    objs = models.zhugedanao_lianjie_tijiao.objects.filter(q)[0:1]
     flag = False
     if objs:
         flag = True
@@ -229,3 +228,57 @@ def shouluTiJiaoRenWu(request):
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# 覆盖查询 判断是否有任务
+@csrf_exempt
+def fuGaiChaXunDecideIsTask(request):
+    now_time = int(time.time())
+    time_stamp30 = now_time + 30
+    q = Q()
+    q.add(Q(is_zhixing=0), Q.AND)
+    q.add(Q(time_stamp__isnull=True) | Q(time_stamp__lte=time_stamp30),Q.AND)
+    objs = models.zhugedanao_fugai_chaxun.objects.filter(q)
+    flag = False
+    if objs:
+        flag = True
+    response.code = 200
+    response.msg = '查询成功'
+    response.data = {'flag':flag}
+    return JsonResponse(response.__dict__)
+
+
+@csrf_exempt
+def fuGaiHuoQuRenWu(request):
+    now_time = int(time.time())
+    time_stamp30 = now_time + 30
+    q = Q()
+    q.add(Q(is_zhixing=0), Q.AND)
+    q.add(Q(time_stamp__isnull=True) | Q(time_stamp__lte=time_stamp30), Q.AND)
+    objs = models.zhugedanao_fugai_chaxun.objects.filter(q)[0:1]
+    if objs:
+        objs[0].time_stamp = time_stamp30
+        objs[0].save()
+        response.data = {
+            'o_id':objs[0].id,
+            'keyword':objs[0].keyword,
+            'search':objs[0].search_engine,
+
+        }
+
+
+@csrf_exempt
+def fuGaiTiJiaoRenWu(request):
+    pass
