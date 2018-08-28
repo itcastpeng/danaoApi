e704c107a3e198ecda5d35d23c0a02bbc7bbc535
zhaoxinpeng <15531506906@sina.cn>
2018-08-28 13:50:49 +0800

    诸葛大脑 用户名转换base64

--- zhugedanao/views_dir/temporary_file/temporary.py
+++ zhugedanao/views_dir/temporary_file/temporary.py
@@ -19,7 +19,6 @@ import urllib.request
 sys.stdout = io.TextIOWrapper(sys.stdout.buffer,encoding='utf8')
 
 def linshi(request):
-    data_list = []
     # for name in ['客户ID230', '客户ID34', '🌻李汉杰👵', '🌿张聪', '卢俊义', '公孙胜', '秦明', '假如', '关胜', '过客❤', 'ju do it',
     #              '西门庆豪|董庆豪|合众', '梦忆🍁', '吴用', '青春不散场@',
     #              '诸葛营销', '武松', '刘鹏', '林敏', '张清', '柴进', '李应', '花荣', '硕子😁 🏀', '胡蓉', '夏宏伟：品牌良医', '许艳', '贺～丹', '余宏亮']:
@@ -29,8 +28,8 @@ def linshi(request):
         encode_username = str(encodestr, encoding='utf-8')
         obj.username = encode_username
         obj.save()
+
     #     decode_username = base64.b64decode(obj.username)
     #     username = str(decode_username, 'utf-8')
-    #     data_list.append(username)
-    # print(data_list)
-    return HttpResponse(','.join(data_list))
+
+    return HttpResponse('完成')
